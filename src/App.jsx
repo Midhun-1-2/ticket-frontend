18fa6c295a53cc87d8ef53edc61cf0fbd7417a17
Midhun <midhuns6371@gmail.com>
2026-07-02 11:42:29 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
 import React, { useEffect } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom'
 import Dashboard from './pages/Dashboard.jsx'
+import Login from './pages/Login.jsx'
 import './style.css'
 import { initApp } from './script.js'
 
-function App() {
+function MainLayout() {
+  return (
+    <div className="app-shell">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
+function App() {
   useEffect(() => {
     initApp()
   }, [])
 
   return (
     <BrowserRouter>
-      <div className="app-shell">
-        <Header />
-        <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='*' element={<div style={{ padding: 24 }}>Page coming soon</div>} />
-        </Routes>
-      </div>
-      <Footer />
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+
+        <Route path='/' element={<Login />} />
+
+        <Route path='*' element={<div style={{ padding: 24 }}>Page coming soon</div>} />
+      </Routes>
     </BrowserRouter>
   )
 }
